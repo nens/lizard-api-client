430ff5701ce3dbedacad5a4635e11d3ebeee90ac
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-07-19 12:49:36 +0200

    fix(getParcels): Export getParcels.

--- src/lizard-api-client.js
+++ src/lizard-api-client.js
@@ -1,5 +1,5 @@
 export { getBootstrap } from './endpoints/bootstrap';
-export { getParcelsByName } from './endpoints/parcels';
+export { getParcels, getParcelsByName } from './endpoints/parcels';
 export { getRasters, getRasterDetail } from './endpoints/rasters';
 export { getTimeseries } from './endpoints/timeseries';
 export { search } from './endpoints/search';
