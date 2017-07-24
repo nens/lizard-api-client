b87bc3aa45ee5a4c70623d5e588a2da9c6b60a2c
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-07-24 15:30:05 +0200

    Everything works like expected

--- src/endpoints/search.js
+++ src/endpoints/search.js
@@ -1,23 +1,47 @@
-import { request } from '../http';
-import SearchResult from '../valueobjects/SearchResult';
+import { endpoint, request } from '../http';
+import Metadata from '../valueobjects/Metadata';
+import Organisation from '../valueobjects/Organisation';
+import { valueObjects } from '../valueobjects/index';
 
-export function search(q, types = null, exclude = []) {
-  let params = {
-    'q': q
-  };
+const DEFAULT_SRID = 4326;
 
-  if (types && types.length) {
-    params.types = types.join();
+export function searchParcels(q, bbox) {
+  const params = { q: q, type: 'parcel' };
+  if (bbox) {
+    params.in_bbox = bbox;
+    params.srid = DEFAULT_SRID;
   }
-
-  if (exclude && exclude.length) {
-    params.exclude = exclude.join();
-  }
-
-  return request('/search/', params).then(
-    (results) =>
-      results.map(
-        (result) => new SearchResult(result)
-      )
+  const url = endpoint('/search/', params);
+  return request(url).then(
+    data => {
+      const listOfPromises = data.results.map(result =>
+        request(result.entity_url)
+      );
+      return Promise.all(listOfPromises).then(allPromisesData => {
+        return allPromisesData.map(result =>
+          processSingleParcelResponse(result, url)
+        );
+      });
+    }
   );
 }
+
+function processSingleParcelResponse(result, url) {
+  const props = result.properties;
+  const processedResult = {
+    geometry: result.geometry,
+    id: result.id,
+    code: props.code,
+    name: props.name,
+    external_id: props.external_id,
+    organisation: props.organisation,
+    metadata: new Metadata({
+      sourceUrl: url,
+      index: null,
+      retrieved: Date.now()
+    })
+  };
+  return new valueObjects.Parcel(processedResult);
+}
+
+
--- src/lizard-api-client.js
+++ src/lizard-api-client.js
@@ -2,4 +2,4 @@ export { getBootstrap } from './endpoints/bootstrap';
 export { getParcels, getParcelsByName } from './endpoints/parcels';
 export { getRasters, getRasterDetail } from './endpoints/rasters';
 export { getTimeseries } from './endpoints/timeseries';
-export { search } from './endpoints/search';
+export { searchParcels } from './endpoints/search';
