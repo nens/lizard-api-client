e8c95416ad9206e4f988f90cdabc96d2b1d0d168
Remco Gerlich <remco.gerlich@nelen-schuurmans.nl>
2017-10-27 10:03:59 +0200

    Rework it a bit, seems to be OK

--- src/redux.js
+++ src/redux.js
@@ -5,39 +5,71 @@
    When to use: when you have simple objects that you can fetch by
    some ID and want to store by some ID.
 
-   Exposes three functions:
-   - makeReducer() -- to make a reducer for the Redux state that will contain the data and metadata
-   - makeGetOrFetch() -- to make a function that can be used to get or fetch the data
-   - makeDeleter() -- similarly, a function that will delete the data items, if needed.
+   Exposes some functions:
 
-   Actions are only for internal use, but they all have structure like:
+   - makeReducer() -- to make a reducer for the Redux state that will
+     contain the data and metadata
 
-   {
-     type: 'OBJECTNAME',
-     action: 'receive',
-     id: 'someid',
-     object: theobject,
-   }
+   - makeGetter() -- to make a function that can be used to get the
+     data from the state (and knows about stale data and such)
 
-   So no separate 'ADD_OBJECTNAME' etc as that isn't necessary for anything.
+   - makeFetcher() -- wraps some function that fetches an object
+     asynchronously so that the right actions are dispatched around it
 
-   Each fetch / receive has its own random string, to check that a received object
-   belongs to the latest fetch (in case of timeouts).
+   - makeDeleter() -- similarly, a function that will delete the data
+     items, if needed.
+
+   - getOrFetch() -- the thing that ties it all together.
+
+   Usage:
+
+   In your reducers:
+
+       const rootReducer = combineReducers({
+           rasters: makeReducer('rasters'),
+           ...
+       });
+
+   In your actions:
+
+      const fetchRaster = makeFetcher('rasters', getRasterDetail);
+
+   In your component's mapStateToProps:
+
+      getRaster: makeGetter(state.rasters),
+
+   In your component's mapDispatchToProps:
+
+      fetchRaster: (id) => fetchRaster(dispatch, id),
+
+   Now use the whole thing as
+
+      getOrFetch(this.getRaster, this.fetchRaster, id);
+
+   Actions are only for internal use, but they all have structure
+   like:
+
+   { type: 'OBJECTNAME', action: 'receive', id: 'someid', object:
+     theobject, }
+
+   So no separate 'ADD_OBJECTNAME' etc as that isn't necessary for
+   anything.
+
+   Each fetch / receive has its own random string, to check that a
+   received object belongs to the latest fetch (in case of timeouts).
 
    Internal state has the form:
 
-   {
-     data: {},
-     metadata: {}
-   }
+   { data: {}, metadata: {} }
 
-   Data is an object that contains the actual data items. Metadata contains extra
-   information like isFetching, receivedTimestamp and error.
+   Data is an object that contains the actual data items. Metadata
+   contains extra information like isFetching, receivedTimestamp and
+   error.
 
-   React components that don't care about the metadata should probably depend on
-   state.objectname.data in connect(), so that they only update when the contents
-   of that change. Access to the data will be by getOrFetchObjectName().
- */
+   React components that don't care about the metadata should probably
+   depend on state.objectname.data in connect(), so that they only
+   update when the contents of that change. Access to the data will be
+   by getOrFetchObjectName().  */
 
 const INITIAL_METADATA = {
   isFetching: false,
@@ -54,21 +86,19 @@ export function makeReducer(objectName) {
       return state;
     }
 
-    let data, metadata;
+    const data = Object.assign({}, state.data);
+    const metadata = Object.assign({}, state.metadata);
 
     switch (action.action) {
       case 'fetch':
-        metadata = {...state.metadata};
-        metadata[action.id] = {...INITIAL_METADATA};
+        metadata[action.id] = Object.assign({}, INITIAL_METADATA);
         metadata[action.id].isFetching = true;
         metadata[action.id].fetchTimestamp = new Date().getTime();
         return {data: state.data, metadata: metadata};
       case 'receive':
         if (state.metadata[action.id]) {
-          metadata = {...state.metadata};
-          metadata[action.id] = {...INITIAL_METADATA};
+          metadata[action.id] = Object.assign({}, INITIAL_METADATA);
           metadata[action.id].receivedTimestamp = new Date().getTime();
-          data = {...state.data};
           data[action.id] = action.object;
           return {data: data, metadata: metadata};
         } else {
@@ -77,8 +107,7 @@ export function makeReducer(objectName) {
         }
       case 'error':
         if (state.metadata[action.id]) {
-          metadata = {...state.metadata};
-          metadata[action.id] = {...INITIAL_METADATA};
+          metadata[action.id] = Object.assign({}, INITIAL_METADATA);
           metadata[action.id].receivedTimestamp = new Date().getTime();
           metadata[action.id].error = action.error;
           return {data: state.data, metadata: metadata};
@@ -87,8 +116,6 @@ export function makeReducer(objectName) {
           return state;
         }
       case 'delete':
-        data = {...state.data};
-        metadata = {...state.metadata};
         delete data[action.id];
         delete metadata[action.id];
         return {data: data, metadata: metadata};
@@ -100,30 +127,28 @@ export function makeReducer(objectName) {
   return reducer;
 }
 
-export function makeGetOrFetch(store, objectName, fetchFunction, params = {}) {
+export function makeGetter(objectState, params = {}) {
   const FETCH_TIMEOUT_SECONDS = params.fetchTimeout || 0;
   const STORE_TIMEOUT_SECONDS = params.storeTimeout || 0;
   const RETRY_ERROR = params.retryError || true;
 
-  const upperObjectName = objectName.toUpperCase();
-  const { data, metadata } = store[objectName];
+  return function getter(id) {
+    const { data, metadata } = objectState;
 
-  return function getOrFetch(dispatch, id) {
-    const returnValue = null;
-    const doFetch = true;
+    let returnValue = null;
+    let shouldFetch = false;
 
     // See if we have it
     if (data[id]) {
       // Yes, return it
       returnValue = data[id];
-      doFetch = false;
 
       // But is it old? Then still get it
       if (STORE_TIMEOUT_SECONDS && metadata[id].receivedTimestamp) {
         const timeStored = (new Date().getTime()) - metadata[id].receivedTimestamp;
 
         if (timeStored > metadata[id].receivedTimestamp) {
-          doFetch = true;
+          shouldFetch = true;
         }
       }
     } else {
@@ -131,45 +156,54 @@ export function makeGetOrFetch(store, objectName, fetchFunction, params = {}) {
       if (!metadata[id] || !metadata[id].isFetching) {
         // We're not yet fetching it. Get it unless there was an error we don't retry.
         if (!RETRY_ERROR && metadata[id] && metadata[id].error) {
-          doFetch = false;
+          shouldFetch = false;
         } else {
-          doFetch = true;
+          shouldFetch = true;
         }
       } else {
         // We *are* fetching it. Does that stop us?
         if (FETCH_TIMEOUT_SECONDS && metadata[id].fetchTimestamp) {
+          // Depends on how long ago it was.
           const timeFetching = (new Date().getTime()) - metadata[id].fetchTimestamp;
 
-          doFetch = timeFetching > FETCH_TIMEOUT_SECONDS;
+          shouldFetch = timeFetching > FETCH_TIMEOUT_SECONDS;
         } else {
           // Yes.
-          doFetch = false;
+          shouldFetch = false;
         }
       }
     }
 
-    // Actually fetch and dispatch actions.
-    if (doFetch) {
-      dispatch({
-        type: upperObjectName,
-        action: 'fetch',
-        id
-      });
+    return {
+      object: returnValue,
+      shouldFetch: shouldFetch
+    };
+  };
+}
 
-      fetchFunction(id).then((object) => dispatch({
+export function makeFetcher(objectName, fetchFunction) {
+  const upperObjectName = objectName.toUpperCase();
+
+  return function fetcher(dispatch, id) {
+    dispatch({
+      type: upperObjectName,
+      action: 'fetch',
+      id
+    });
+
+    fetchFunction(id).then(
+      (object) => dispatch({
         type: upperObjectName,
         action: 'receive',
         id,
         object
-      })).error((error) => dispatch({
+      }),
+      (error) => dispatch({
         type: upperObjectName,
         action: 'error',
         id,
         error
       }));
-    }
-
-    return returnValue;
   };
 }
 
@@ -181,4 +215,15 @@ export function makeDeleter(objectName) {
       id
     });
   };
+}
+
+export function getOrFetch(getter, fetcher, id) {
+  // Helper function that combines the parts.
+  const { object, shouldFetch } = getter(id);
+
+  if (shouldFetch) {
+    fetcher(id);
+  }
+
+  return object;
 }
